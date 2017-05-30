e657661b26b8493399ee770245b6969135b7ab0f
Leonor Martins <leonorfmartins@gmail.com>
2017-05-30 09:39:07 +0200

    Add dummy dashboard

--- src/client/assets/javascripts/app/routes.js
+++ src/client/assets/javascripts/app/routes.js
@@ -3,10 +3,11 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 
 import App from './App';
 import NotFoundView from 'components/NotFound';
+import Dashboard from 'components/Dashboard';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={App} />
+    <IndexRoute component={Dashboard} />
     <Route path="404" component={NotFoundView} />
     <Redirect from="*" to="404" />
   </Route>
---
+++ src/client/assets/javascripts/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import * as d3 from 'd3';
+
+export default class Dashboard extends Component {
+ render() {
+   <div className="chart">
+     <span>Put great chart here</span>
+   </div>;
+ } 
+} 
